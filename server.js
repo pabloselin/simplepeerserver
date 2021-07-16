ccf27e39e99c82c4f3a36a2479a581a61b68f38b
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2021-07-16 17:16:40 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const { PeerServer } = require("peer");
+
+const peerServer = PeerServer({
+	port: 9000,
+	path: "/cenexxxpeerserver",
+	ssl: {
+		key: fs.readFileSync(
+			"/etc/letsencrypt/live/radio.cenexxx.cl/privkey.pem"
+		),
+		cert: fs.readFileSync("/etc/letsencryp/live/radio.cenexxx.cl/cert.pem"),
+	},
+});
