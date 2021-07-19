14284c7caca3acc40429ab2486a5d69f42544e87
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2021-07-19 11:41:50 +0200

    server change

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const { PeerServer } = require("peer");
 const peerServer = PeerServer({
 	port: 9000,
 	path: "/cenexxxpeerserver",
-	key: "cenexxx",
 	ssl: {
 		key: fs.readFileSync(
 			"/etc/letsencrypt/live/radio.cenexxx.cl/privkey.pem"
